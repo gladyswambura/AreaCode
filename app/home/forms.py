0db08a376270de18ca89a542fa68531ee716b02b
john.njau <john.njoroge@student.moringaschool.com>
2022-05-17 09:29:07 +0300

    database : database schema

--- app/home/forms.py
+++ app/home/forms.py
@@ -3,7 +3,7 @@ from wtforms import SubmitField, StringField, PasswordField, BooleanField, Valid
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=200)])
     submit =SubmitField('Submit')
     
 class CommentForm(FlaskForm):
