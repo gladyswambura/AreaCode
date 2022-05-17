c5a2a9e68980c628f4055e1ea12954d34e124db2
gladyswambura <gladyswahito7@gmail.com>
2022-05-17 22:35:53 +0300

    fetching

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ class DevConfig(Config):
     DEBUG=True
 
 class ProdConfig(Config):
-<<<<<<< HEAD
     '''
     Production  configuration child class
     Args:
@@ -33,17 +32,6 @@ class DevConfig(Config):
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@127.0.0.1:5432/areacodelogin'
 
-=======
-    uri = os.getenv('DATABASE_URL')
-    if uri and uri.startswith('postgres://'):
-        uri = uri.replace('postgres://', 'postgresql://', 1)
-
-    SQLALCHEMY_DATABASE_URI = uri
-
-class TestConfig(Config):
-    pass
-
->>>>>>> f9859606b796d9aff5d8ff117ed11b103f9ebe12
 config_options = {
     'development': DevConfig,
     'production': ProdConfig,
