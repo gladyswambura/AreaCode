c6d967ea10935761a48d2c6cdfecadf8de7e43eb
FloWambui <gflorencewambui@gmail.com>
2022-05-17 06:17:44 +0300

    add the sign=up form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,17 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, BooleanField
-from wtforms.validators import InputRequired,Email,EqualTo
+from wtforms import StringField,PasswordField,SubmitField, BooleanField,SelectField
+from wtforms.validators import DataRequired,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
 
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
-    username = StringField('Enter your username',validators = [InputRequired()])
-    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
-    submit = SubmitField('Sign Up')
+    firstname = StringField('First Name',validators=[DataRequired()])
+    lastname = StringField('Last Name',validators=[DataRequired()])
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    username = StringField('Enter your username',validators = [DataRequired()])
+    location = SelectField('Location', coerce=int,choices=[(0, 'Please Select...'), (1, 'Ruaka'), (2, 'Muthaiga'), (3, 'Waiyaki Way'),(4, 'Ngong Road'), (5, 'Buruburu')],validators=[DataRequired()])
+    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Password',validators = [DataRequired()])
+    terms = BooleanField('I have read and agreed with the terms of service and <br> our privacy policy.')
+    submit = SubmitField('SIGN UP')
 
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
