7be9672342e47352aad551579a5a54a226da47d0
FloWambui <gflorencewambui@gmail.com>
2022-05-18 09:26:43 +0300

    add the login and routing functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,7 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
-            return redirect(request.args.get("next") or url_for("main.index"))
+            return redirect(request.args.get("next") or url_for("home.home"))
     title = "Login"
     return render_template("auth/login.html", login_form = login_form, title = title)
 
@@ -38,4 +38,4 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("main.index"))@auth.route('/logout')
+    return redirect(url_for("main.index"))
